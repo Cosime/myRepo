a811e25b1dc91fe754c3110276acf8f787cc4e8c
Simon Winters <smwinters42@students.tntech.edu>
2019-02-01 19:02:12 -0600

    add function

--- app/commands/first.rb
+++ app/commands/first.rb
@@ -26,7 +26,7 @@ module First
       attempt = 1
       end
     output = ""
-    output +=  "You killed jad #{attempt} times. (while on task)\n"
+
     totalpets = 0
     attempt.times do |i|
       x = rand(100)
@@ -36,19 +36,17 @@ module First
          totalpets += 1
       end
       if (y == 100 && x != 50)
-         output += "You turned in you cape for pet after kill #{i+1}!\n"
-         totalpets += 1
+        output += "You turned in you cape for pet after kill #{i+1}!\n"
+        totalpets += 1
       end
       if (x == 50 && y == 100)
-        "You got pet, and turned in your cape for pet on kill #{i+1}!\n"
-         totalpets += 2
+        output += "You got pet, and turned in your cape for pet on kill #{i+1}!\n"
+        totalpets += 2
       end
     end
-      output +=  "You got #{totalpets} pets!\n"
-    if (totalpets == 0)
-      output +=  ":(\n"
-    end
-    send_message(@bot.bot_id, output)
+      output =  "You killed jad #{attempt} times. (while on task)\nYou got #{totalpets} pets!\n" + output
+      send_message(@bot.bot_id, output) unless (totalpets == 0)
+      send_message(@bot.bot_id, "You didn't get any pets :(")
   end
 
 end
