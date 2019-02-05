6840430578f8536f68923ff60736b9f43dc5b95d
Simon Winters <smwinters42@students.tntech.edu>
2019-02-04 21:06:50 -0600

    lol

--- app/commands/first.rb
+++ app/commands/first.rb
@@ -59,7 +59,7 @@ module First
     data = JSON.parse(Net::HTTP.get(uri))
 
     champID = findchamp(parameters[0])
-    if (champID == nil) then return send_message(@bot.bot_id, "invalid champion name")
+    if (champID == nil) then return send_message(@bot.bot_id, "invalid champion name") end
     a = data.select { |x| x['_id']['championId'] == champID}
     winrate = a.first['winRate']
 
@@ -87,6 +87,6 @@ private
   p champs
   selected = champs.select { |x| x["name"].downcase == championName}
   p selected
-  if (selected.legnth == 0) then return nil
+  if (selected.legnth == 0) then return nil end
   selected.first["id"]
   end
